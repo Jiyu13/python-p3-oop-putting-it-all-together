6a557691b35e511c798348555f42095724b5e175
Ziru Zou <jiyucoding@yahoo.com>
2023-02-20 02:45:39 -0500

    set page_count property default value to None for when get_page_count() is called

--- lib/book.py
+++ lib/book.py
@@ -2,16 +2,21 @@
 
 class Book:
     
-    def __init__(self, title):
+    def __init__(self, title, page_count=None):
         self.title = title
+        self._page_count = page_count
 
     
     def get_page_count(self):
         return self._page_count
     
-    def set_page_count(self, page_count):
-        self._page_count = page_count
+    def set_page_count(self, count):
+        if type(count) != int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = count   # count is a param we pass in
     
 
+    # page_count here is a property
     page_count = property(get_page_count, set_page_count)
 
