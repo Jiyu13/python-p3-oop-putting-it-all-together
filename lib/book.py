f6f22114d65bc73601a1c9b6f810db595302a18a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-20 01:34:38 -0500

    create page_count property with given getter and setter

--- lib/book.py
+++ lib/book.py
@@ -5,4 +5,13 @@ class Book:
     def __init__(self, title):
         self.title = title
 
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        self._page_count = page_count
+    
+
+    page_count = property(get_page_count, set_page_count)
 
