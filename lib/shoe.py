fbd6aad743fbdf7fa5c093bc20a5becf60f2f614
Ziru Zou <jiyucoding@yahoo.com>
2023-02-20 03:36:23 -0500

    create color and size properties with getter and setter

--- lib/shoe.py
+++ lib/shoe.py
@@ -5,3 +5,31 @@ class Shoe:
     
     def __init__(self, brand):
         self.brand = brand
+        self._color = None  # set default value for when get_size() is called, if not error
+        self._size = None
+
+
+    def get_color(self):
+        return self._color
+
+
+    def set_color(self, color):
+        self._color = color
+
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, shoe_size):
+        if type(shoe_size) != int:
+            print("size must be an integer")
+        else:
+            self._size = shoe_size   # count is a param we pass in
+
+
+
+
+
+    color = property(get_color, set_color)
+    size = property(get_size, set_size)
+    
