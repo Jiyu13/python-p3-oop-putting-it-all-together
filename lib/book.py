f386a62f7299de0d3e4682128e9229b7d8eacfb8
Ziru Zou <jiyucoding@yahoo.com>
2023-02-26 07:44:03 -0500

    redo

--- lib/book.py
+++ lib/book.py
@@ -2,24 +2,40 @@
 
 class Book:
     
-    def __init__(self, title, page_count=None):
+    def __init__(self, title, page_count=0):
         self.title = title
-        self._page_count = page_count   # set default value for when get_page_count() is called, if not error
+        self.page_count = page_count     # set it to 0 by default
 
-    
+
+    def get_author(self):
+        return self.author
+
+    def set_author(self, author_name):
+        self.author = author_name
+
+
     def get_page_count(self):
         return self._page_count
+
     
-    def set_page_count(self, count):
-        if type(count) != int:
-            print("page_count must be an integer")
+    def set_page_count(self, pages):
+        if type(pages) == int:
+            self._page_count = pages
         else:
-            self._page_count = count   # count is a param we pass in
+            print("page_count must be an integer")
+
     
-
-    def turn_page(self):
-        print("Flipping the page...wow, you read fast!")
-
-    # page_count here is a property
     page_count = property(get_page_count, set_page_count)
 
+
+    def get_genre(self):
+        return self.genre
+    
+
+    def set_genre(self, genre):
+        self.genre = genre
+
+
+    # define instance method
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -5,34 +5,36 @@ class Shoe:
     
     def __init__(self, brand):
         self.brand = brand
-        self._color = None  # set default value for when get_size() is called, if not error
-        self._size = None
-        self.condition = None
+        self.size = 0   # set to 0 by default, or AttributeError: 'Shoe' object has no attribute '_size'
 
-
-    def get_color(self):
-        return self._color
-
+    
 
     def set_color(self, color):
-        self._color = color
+        self.color = color
 
-    
+
     def get_size(self):
         return self._size
+
     
-    def set_size(self, shoe_size):
-        if type(shoe_size) != int:
-            print("size must be an integer")
+    def set_size(self, size_val):
+        if type(size_val) == int:
+            self._size = size_val
         else:
-            self._size = shoe_size   
-        
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+
+    def set_material(self, material):
+        self.material = material
+    
+
+    def set_condition(self, condition):
+        self.condition = condition
+
 
     def cobble(self):
         print("Your shoe is as good as new!")
         self.condition = "New"
 
-
-    color = property(get_color, set_color)
-    size = property(get_size, set_size)
-    
